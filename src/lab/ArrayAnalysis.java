c63501153bc5f869bfa0b38b11e0f899699d11cd
1Door <89485167+1Door@users.noreply.github.com>
2022-10-12 13:34:16 -0500

    comments

--- src/lab/ArrayAnalysis.java
+++ src/lab/ArrayAnalysis.java
@@ -32,10 +32,10 @@ public class ArrayAnalysis {
 	// #2
 	public static void printArray(int[][] arr) {
 
-		for (int[] row : arr) {
-			for (int element : row) {
+		for (int[] row : arr) {  
+			for (int element : row) {  //runs through all the elements
 
-				System.out.print(element + " ");
+				System.out.print(element + " "); //prints out the element
 
 			}
 			System.out.println();
@@ -47,12 +47,12 @@ public class ArrayAnalysis {
 	// #3
 	public static int sumTotal(int[][] arr) {
 
-		int total = 0;
+		int total = 0; //total
 
 		for (int i = 0; i < arr.length - 1; i++) {
-			for (int j = 0; j < arr[i].length - 1; j++) {
+			for (int j = 0; j < arr[i].length - 1; j++) { //grabs each element
 
-				total += arr[i][j];
+				total += arr[i][j]; //adds the current element to the total
 
 			}
 
@@ -65,19 +65,19 @@ public class ArrayAnalysis {
 	public static double average(int[][] arr) {
 
 		int total = 0;
-		double count = 0;
+		double count = 0; //variables
 
-		for (int i = 0; i < arr.length - 1; i++) {
+		for (int i = 0; i < arr.length - 1; i++) { //loops through all elements
 			for (int j = 0; j < arr[i].length - 1; j++) {
 
-				total += arr[i][j];
-				count++;
+				total += arr[i][j]; //gets total
+				count++; //adds  to the count for the average
 			}
 
 		}
 		
 		
-		return total / count;
+		return total / count; //gets the average
 	}
 
 	// #5
@@ -85,11 +85,11 @@ public class ArrayAnalysis {
 
 		int min = 240000000;
 		for (int[] row : arr) {
-			for (int element : row) {
+			for (int element : row) { //loops through all elements
 
-				if (element < min) {
+				if (element < min) { //if it is smaller  than current  num
 
-					min = element;
+					min = element; //then make this the new smallest
 
 				}
 				
@@ -107,11 +107,11 @@ public class ArrayAnalysis {
 
 		int max = -240000000;
 		for (int[] row : arr) {
-			for (int element : row) {
+			for (int element : row) { //loops through all elements
 
-				if (element > max) {
+				if (element > max) { //if it is greater than the old max
 
-					max = element;
+					max = element; //make it the new max
 
 				}
 				
@@ -120,27 +120,27 @@ public class ArrayAnalysis {
 			
 		}
 			
-		return max;
+		return max; //returns
 	}
 
 	// #7
 	public static int[][] diffArrays(int[][] arr1, int[][] arr2) {
 
 		int count = 0;
-		if(arr1.length == arr2.length && arr1[0].length == arr2[0].length) {
-			int[][] arr = new int [arr1.length][arr1[0].length];
+		if(arr1.length == arr2.length && arr1[0].length == arr2[0].length) { //checks to see if arrays are equal
+			int[][] arr = new int [arr1.length][arr1[0].length]; //new array
 			
 			for(int i = 0; i < arr1.length; i++)	
 			{
 				
 				
-				for(int j = 0; j < arr1[0].length; j++) {
+				for(int j = 0; j < arr1[0].length; j++) {  //2 loops to loop through all the elements
 					
 					
-					arr[i][j] = arr1[i][j] - arr2[i][j];
+					arr[i][j] = arr1[i][j] - arr2[i][j]; //subtracts the elements
 					
-					System.out.println("Element " + count + ": "+ arr[i][j]);
-					 count++;
+					System.out.println("Element " + count + ": "+ arr[i][j]); //prints out the array
+					 count++; //keeps tract of the element to print
 				}
 				
 				
@@ -159,26 +159,26 @@ public class ArrayAnalysis {
 	
 	public static int[][] transposeRow(int[][] arr1, int dist) {
 		
-		int[][] newArr  = new int[arr1.length][arr1[0].length];
+		int[][] newArr  = new int[arr1.length][arr1[0].length];  //creates the second array
 		
-		for  (int row = 0; row < arr1.length - 1; row++) {
+		for  (int row = 0; row < arr1.length - 1; row++) {  
 			
-			newArr[row+1] = arr1[row];
+			newArr[row+dist] = arr1[row]; //moves the  row  down by dist
 			
 			}		
 		
-			newArr[0] = arr1[arr1.length - 1];
+			newArr[0] = arr1[arr1.length - 1]; 
 		
 		return newArr;
 	}
 
 	// #9
-	public static long sum3D(int[][][] arr) {
+	public static long sum3D(int[][][] arr) { 
 
 		long total = 0;
 		
-		for(int i = 0;  i < arr.length - 1;  i++) {
-			total =+ (long) ArrayAnalysis.sumTotal(arr[i]);
+		for(int i = 0;  i < arr.length - 1;  i++) { //1 more loop for the 3rd dimension
+			total =+ (long) ArrayAnalysis.sumTotal(arr[i]); //uses the other method and adds it to the total
 			
 			
 		}
@@ -189,10 +189,10 @@ public class ArrayAnalysis {
 	// #10
 	public static double average3D(int[][][] arr) {
 
-		long total = sum3D(arr);
-		int num = (arr.length) * (arr[0].length) * (arr[0][0].length);
+		long total = sum3D(arr); //takes the sum from previous method 
+		int num = (arr.length) * (arr[0].length) * (arr[0][0].length); //gets the number of all the elements in the 3d array
 		
-		double average = (double) total / (double) num;
+		double average = (double) total / (double) num; //divides the total by the numb
 		
 		
 		return average;
